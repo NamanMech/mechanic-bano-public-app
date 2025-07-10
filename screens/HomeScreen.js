bde0183aed6a50719655a782c1cd6088c3a8b94a
NamanMech <mechanicseekho@gmail.com>
2025-07-10 15:19:55 +0530

    Create HomeScreen.js

---
+++ screens/HomeScreen.js
@@ -0,0 +1,25 @@
+// screens/HomeScreen.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Mechanic Bano Public App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#121212',
+  },
+  title: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
