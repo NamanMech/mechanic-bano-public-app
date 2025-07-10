915ea6a7fd51903606b8b6f70f16b7a257f813f5
NamanMech <mechanicseekho@gmail.com>
2025-07-10 15:40:18 +0530

    Update firebaseConfig.js

--- utils/firebaseConfig.js
+++ utils/firebaseConfig.js
@@ -1,13 +1,13 @@
-// utils/firebaseConfig.js
+// src/utils/firebaseConfig.js
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: "AIzaSyBc1bRjPiMC0LCCht-wZGTDY5RTLC_Eo0U",
+  authDomain: "mechanicbanopublic.firebaseapp.com",
+  projectId: "mechanicbanopublic",
+  storageBucket: "mechanicbanopublic.firebasestorage.app",
+  messagingSenderId: "303726026304",
+  appId: "1:303726026304:web:7db40b059ff042252209a5"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
