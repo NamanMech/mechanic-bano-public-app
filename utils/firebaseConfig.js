647ccb179f415aecc11d4d3349202f85de3d2dab
NamanMech <mechanicseekho@gmail.com>
2025-07-10 18:01:51 +0530

    Update firebaseConfig.js

--- utils/firebaseConfig.js
+++ utils/firebaseConfig.js
@@ -1,4 +1,3 @@
-// utils/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
@@ -6,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBc1bRjPiMC0LCCht-wZGTDY5RTLC_Eo0U",
   authDomain: "mechanicbanopublic.firebaseapp.com",
   projectId: "mechanicbanopublic",
-  storageBucket: "mechanicbanopublic.appspot.com",  // ✅ FIXED
+  storageBucket: "mechanicbanopublic.appspot.com",
   messagingSenderId: "303726026304",
   appId: "1:303726026304:web:7db40b059ff042252209a5"
 };
