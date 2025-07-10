022f1cabb65af9237a415d89b6d34b0ccaea9b99
NamanMech <mechanicseekho@gmail.com>
2025-07-10 18:17:58 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+// App.js
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
+import GoogleLoginScreen from './screens/GoogleLoginScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={GoogleLoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
